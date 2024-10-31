f0bfef46f8b781f65dd9f42c6c2528770b74de33
muhammad-ahmed-janjua <muhammadahmedjanjua.02@gmail.com>
2024-11-01 00:19:50 +1100

    updated with Nav, Hero and Home Card Components

--- job-page-project/src/App.vue
+++ job-page-project/src/App.vue
@@ -1,3 +1,11 @@
+<script setup>
+  import navbar from './components/navbar.vue';
+  import Hero from './components/Hero.vue';
+  import HomeCards from './components/HomeCards.vue';
+</script>
+
 <template>
-  <h1>Vue Jobs</h1>
+  <navbar />
+  <Hero />
+  <HomeCards />
 </template>
