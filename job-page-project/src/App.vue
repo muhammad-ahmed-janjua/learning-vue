857b39760dd79205c8ef2053135f5b1498946683
muhammad-ahmed-janjua <muhammadahmedjanjua.02@gmail.com>
2024-11-01 03:10:50 +1100

    update app

--- job-page-project/src/App.vue
+++ job-page-project/src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
-  import navbar from './components/navbar.vue';
-  import Hero from './components/Hero.vue';
-  import HomeCards from './components/HomeCards.vue';
+  import navbar from './components/Navbar.vue';
+  import { RouterView } from 'vue-router';
 </script>
 
 <template>
   <navbar />
-  <Hero />
-  <HomeCards />
+  <RouterView />
 </template>
