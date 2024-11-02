ca05209f5e88ff4b76e6d3d9c33005e0886c5551
muhammad-ahmed-janjua <muhammadahmedjanjua.02@gmail.com>
2024-11-03 03:28:13 +1100

    fixed server response

--- job-page-project/src/components/JobListings.vue
+++ job-page-project/src/components/JobListings.vue
@@ -3,7 +3,11 @@
 	import { reactive, defineProps, onMounted } from 'vue';
 	import { RouterLink } from 'vue-router';
 	import axios from 'axios';
-	const jobs = ref([]);
+	
+	const state = reactive({
+		jobs: [],
+		isLoading: true
+	})
 
 	defineProps ({
 		limit: Number,
@@ -16,9 +20,11 @@
 	onMounted(async () => {
 		try {
 			const response = await axios.get('http://localhost:5001/jobs');
-			jobs.value = response.data;
+			state.jobs = response.data;
 		} catch (error) {
 			console.error('Error fetching jobs', error);
+		} finally {
+			state.isLoading = false;
 		}
 	})
 </script>
@@ -30,7 +36,7 @@
 				Browse Jobs
 			</h2>	
 			<div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-				<JobListing v-for="job in jobs.slice(0, limit || jobs.lenght)" :key="job.id" :job="job"/>
+				<JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job"/>
 			</div>
 		</div>
 	</section>
