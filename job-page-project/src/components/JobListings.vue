376d63de8da21e6ecacfc37f671bf5807b9edc20
muhammad-ahmed-janjua <muhammadahmedjanjua.02@gmail.com>
2024-11-03 03:20:11 +1100

    added json server functionality

--- job-page-project/src/components/JobListings.vue
+++ job-page-project/src/components/JobListings.vue
@@ -1,15 +1,24 @@
 <script setup>
-	import jobData from '@/jobs.json'
 	import JobListing from './JobListing.vue';
-	import { ref, defineProps } from 'vue';
-
-	const jobs = ref(jobData);
+	import { reactive, defineProps, onMounted } from 'vue';
+	import { RouterLink } from 'vue-router';
+	import axios from 'axios';
+	const jobs = ref([]);
 
 	defineProps ({
 		limit: Number,
 		showButton: {
 			type: Boolean,
 			default: false,
+		}
+	})
+
+	onMounted(async () => {
+		try {
+			const response = await axios.get('http://localhost:5001/jobs');
+			jobs.value = response.data;
+		} catch (error) {
+			console.error('Error fetching jobs', error);
 		}
 	})
 </script>
@@ -20,17 +29,17 @@
 			<h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
 				Browse Jobs
 			</h2>	
-			<div class="grid grid-cols-1 md:grid-cols-3 gap-6" style="display: grid;">
+			<div class="grid grid-cols-1 md:grid-cols-3 gap-6">
 				<JobListing v-for="job in jobs.slice(0, limit || jobs.lenght)" :key="job.id" :job="job"/>
 			</div>
 		</div>
 	</section>
 
 	<section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-      <a
-        href="/jobs"
+      <RouterLink
+        to="/jobs"
         class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
+        >View All Jobs
+	  </RouterLink>	
     </section>
 </template>
